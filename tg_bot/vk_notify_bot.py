26c85df740fc06a12a2e77eb365737e85295884b
loverfish <blackmemberb@gmail.com>
2021-06-02 14:37:54 +0300

    Added multithreading function

--- tg_bot/vk_notify_bot.py
+++ tg_bot/vk_notify_bot.py
@@ -1,4 +1,6 @@
 import logging
+from threading import Thread
+
 from telegram.ext import Updater, CommandHandler
 from time import sleep
 
@@ -18,19 +20,23 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-def online(update, context):
-    user_id = context.args[0] if len(context.args) == 1 else '111297977'
+def notify_user_is_online(update, context):
+    user_id = context.args[0] if len(context.args) == 1 else user_id_vk
     while True:
         response = get_user_info_vk(user_id)
         if response.get('online'):
             message = f"Пользователь {response.get('first_name')} {response.get('last_name')} появился в сети вк"
             context.bot.send_message(chat_id=update.effective_chat.id, text=message)
             break
-        sleep(180)
+        sleep(80)
 
 
-def report(update, context):
-    user_id, duration = '111297977', 1
+def online_stream(update, context):
+    Thread(target=notify_user_is_online, args=(update, context)).start()
+
+
+def report_user_online(update, context):
+    user_id, duration = user_id_vk, 10
     if len(context.args) == 2:
         user_id = context.args[0]
         duration = int(context.args[1])
@@ -40,9 +46,13 @@ def report(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
+def report_stream(update, context):
+    Thread(target=report_user_online, args=(update, context)).start()
+
+
 start_handler = CommandHandler('start', start)
-online_handler = CommandHandler('online', online)
-report_handler = CommandHandler('report', report)
+online_handler = CommandHandler('online', online_stream)
+report_handler = CommandHandler('report', report_stream)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(online_handler)
